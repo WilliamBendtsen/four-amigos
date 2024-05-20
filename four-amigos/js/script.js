0bc326e143b0e6bb811ef030b9d666ce13ef0728
WilliamBendtsen <eaa23wfb@students.eaaa.dk>
2024-05-20 10:25:47 +0200

    404

---
+++ four-amigos/js/script.js
@@ -0,0 +1,11 @@
+function redirectToIndex() {
+    window.location.href = "index.html";
+}
+
+function redirectToMenu() {
+    window.location.href = "menu.html";
+}
+
+function redirectToBestil() {
+    window.location.href = "bestil.html";
+}
