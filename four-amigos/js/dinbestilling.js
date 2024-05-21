59ab045391178beecda6418e45be24a2b7c7c12c
Proteinbro <eaa23ckkn@students.eaaa.dk>
2024-05-21 13:52:24 +0200

    ANother one - bestilling
    Ja tak

--- four-amigos/js/dinbestilling.js
+++ four-amigos/js/dinbestilling.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let removeButton = document.querySelector('.remove-button');
-    let elementsToRemove = document.querySelectorAll('.afhentning-plads, .taco4stk-overskrift, .taco4stk-underskrift, .afhentning-taco, .taco4stk-pris, .betalnu, .remove-button');
-    let elementsToAdd = document.querySelector('.')
+    let removeButton1 = document.querySelector('.remove-button');
+    let removeButton2 = document.querySelector('.fjern');
+    let elementsToRemove = document.querySelectorAll('.afhentning-plads, .taco4stk-overskrift, .taco4stk-underskrift, .afhentning-taco, .taco4stk-pris, .betalnu, .remove-button, .fjern');
+    let elementsToAdd = document.querySelector('.fjernet')
 
-    removeButton.addEventListener('click', function() {
+    removeButton1.addEventListener('click', function() {
         elementsToAdd.classList.add('popup');
         elementsToRemove.forEach(function(element) {
             element.remove();
 
         });
     });
+
+    removeButton2.addEventListener('click', function() {
+        elementsToAdd.classList.add('popup');
+        elementsToRemove.forEach(function(element) {
+            element.remove();
+
+        });
+    });
+
 });
